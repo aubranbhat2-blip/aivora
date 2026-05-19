69ead7e3c87365de5f60b52c111749ba7ea3cbff
aubranbhat2-blip <aubranbhat2@gmail.com>
2026-05-19 15:06:05 +0530

    fixed live backend url

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -23,7 +23,7 @@ export default function SignupPage() {
     setStatus("");
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/signup", {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/signup`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
